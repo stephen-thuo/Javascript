28255709bdbe3e0d4865a92b1bfd5b02385b28df
stephen-thuo <sthuo95@gmail.com>
2025-05-10 18:52:28 +0300

    javascript

---
+++ Javascript Assignment/index.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("mybutton");
+const paragraph = document.getElementById("myparagraph");
+
+button.addEventListener("click", () => {
+paragraph.classList.toggle("text");
+});
